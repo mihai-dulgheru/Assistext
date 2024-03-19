ee8dce62cac7fb169e36cc85e581bbd8143bbf49
Mihai-Nicolae Dulgheru <dulgherumihai19@stud.ase.ro>
2024-03-20 00:45:02 +0200

    Refactor speech option input fields in SettingsScreen

--- components/SettingsScreen.js
+++ components/SettingsScreen.js
@@ -1,4 +1,3 @@
-// import {Picker} from '@react-native-picker/picker';
 import React from 'react';
 import {StyleSheet, Text, TextInput, View} from 'react-native';
 import {borderRadius, colors} from '../theme';
@@ -8,47 +7,35 @@ export default function SettingsScreen() {
   const {speechOptions, setSpeechOptions} = useSpeechContext();
 
   const updateSpeechOption = (option, value) => {
-    const settingValue = isNaN(parseFloat(value)) ? 0 : parseFloat(value);
-    setSpeechOptions({...speechOptions, [option]: settingValue});
+    const settingValue = parseFloat(value) || 1;
+    setSpeechOptions((prevOptions) => ({
+      ...prevOptions,
+      [option]: settingValue,
+    }));
   };
 
   return (
     <View style={styles.container}>
-      {/* <Text style={styles.label}>Limba (Language):</Text>
-      <View style={styles.pickerContainer}>
-        <Picker
-          selectedValue={speechOptions.language}
-          style={styles.picker}
-          onValueChange={(itemValue) =>
-            updateSpeechOption('language', itemValue)
-          }>
-          <Picker.Item label='Română (România)' value='ro-RO' />
-          <Picker.Item label='Engleză (SUA)' value='en-US' />
-        </Picker>
-      </View> */}
-
       <Text style={styles.label}>Înălțimea tonului de voce (Pitch):</Text>
       <TextInput
         style={styles.input}
-        keyboardType='numeric'
-        onChangeText={(value) => updateSpeechOption('pitch', value)}
-        value={speechOptions.pitch.toString()}
+        keyboardType='decimal-pad'
+        onEndEditing={(e) => updateSpeechOption('pitch', e.nativeEvent.text)}
+        defaultValue={speechOptions.pitch.toString()}
       />
-
       <Text style={styles.label}>Viteza de vorbire (Rate):</Text>
       <TextInput
         style={styles.input}
-        keyboardType='numeric'
-        onChangeText={(value) => updateSpeechOption('rate', value)}
-        value={speechOptions.rate.toString()}
+        keyboardType='decimal-pad'
+        onEndEditing={(e) => updateSpeechOption('rate', e.nativeEvent.text)}
+        defaultValue={speechOptions.rate.toString()}
       />
-
       <Text style={styles.label}>Volumul (Volume):</Text>
       <TextInput
         style={styles.input}
-        keyboardType='numeric'
-        onChangeText={(value) => updateSpeechOption('volume', value)}
-        value={speechOptions.volume.toString()}
+        keyboardType='decimal-pad'
+        onEndEditing={(e) => updateSpeechOption('volume', e.nativeEvent.text)}
+        defaultValue={speechOptions.volume.toString()}
       />
     </View>
   );
